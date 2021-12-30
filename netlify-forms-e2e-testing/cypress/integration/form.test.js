5944cfe28a15cd0c72807d426f28c981536cf39d
sidyes <stefan.herpich@t-online.de>
2021-12-30 11:46:25 +0100

    feat: add cypress

---
+++ netlify-forms-e2e-testing/cypress/integration/form.test.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+
+function fillWithValidData() {
+  cy.get("[data-cy=firstName]").type("Simpel");
+  cy.get("[data-cy=lastName]").type("Web");
+  cy.get("[data-cy=category]").select("A");
+}
+
+function assertCorrectRequest(intercept) {
+  expect(intercept.request.method).to.equal("POST");
+  expect(intercept.request.headers).to.include({
+    "content-type": "application/x-www-form-urlencoded",
+  });
+  expect(intercept.request.body).to.contain("form-name=contact");
+}
+
+describe("Form Test", () => {
+  beforeEach(() => {
+    // visit uses baseUrl from cypress.json and can be overwritten e.g. by setting CYPRESS_BASE_URL env
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        cy.spy(win.console, "log").as("consoleLog");
+      },
+    });
+  });
+
+  // Does only work on remote URL with a registered netlify form
+  it("should successfully submit the contact form to netlify", () => {
+    cy.intercept("POST", "/").as("postForm");
+
+    fillWithValidData();
+
+    cy.get("[data-cy=submit]").click();
+
+    cy.wait("@postForm")
+      .then((intercept) => {
+        assertCorrectRequest(intercept);
+      })
+      .then(() => {
+        var expected = {
+          firstName: "Simpel",
+          lastName: "Web",
+          category: "A",
+        };
+        cy.get("@consoleLog").should("be.calledWith", JSON.stringify(expected));
+        cy.get("@consoleLog").should(
+          "be.calledWith",
+          "Message successfully sent."
+        );
+      });
+  });
+});
--- netlify-forms-e2e-testing/src/components/form.js
+++ netlify-forms-e2e-testing/src/components/form.js
@@ -59,6 +59,7 @@ const FormComponent = () => {
           {...register("firstName")}
           placeholder="First name"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          data-cy="firstName"
         />
       </div>
 
@@ -73,6 +74,7 @@ const FormComponent = () => {
           {...register("lastName")}
           placeholder="Last name"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          data-cy="lastName"
         />
       </div>
 
@@ -86,6 +88,7 @@ const FormComponent = () => {
         <select
           {...register("category")}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          data-cy="category"
         >
           <option value="">Select...</option>
           <option value="A">Category A</option>
@@ -98,6 +101,7 @@ const FormComponent = () => {
       <input
         type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full"
+        data-cy="submit"
       />
     </form>
   );
