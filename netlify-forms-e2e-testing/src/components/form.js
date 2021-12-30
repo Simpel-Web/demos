8b87e650ce6dfec5429015dd5f61572e6b9b016e
sidyes <stefan.herpich@t-online.de>
2021-12-30 10:45:32 +0100

    feat: setup form

--- netlify-forms-e2e-testing/src/components/form.js
+++ netlify-forms-e2e-testing/src/components/form.js
@@ -2,10 +2,19 @@ import * as React from "react";
 
 import { useForm } from "react-hook-form";
 
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join("&");
+}
+
 const FormComponent = () => {
   const { register, handleSubmit } = useForm();
 
+  const [result, setResult] = React.useState("");
+
   const sendForm = (formData, event) => {
+    setResult(JSON.stringify(formData));
     event.preventDefault();
     fetch("/", {
       method: "POST",
@@ -16,7 +25,6 @@ const FormComponent = () => {
       }),
     })
       .then((response) => {
-        reset();
         if (response.ok) {
           console.log("Message successfully sent.");
         } else {
@@ -33,21 +41,64 @@ const FormComponent = () => {
       data-netlify="true"
       netlify-honeypot="bot-field"
       onSubmit={handleSubmit(sendForm)}
+      className="w-[600px]"
     >
       <p class="hidden">
         <label>
           Don’t fill this out if you’re human: <input name="bot-field" />
         </label>
       </p>
-      <input {...register("firstName")} placeholder="First name" />
-      <input {...register("lastName")} placeholder="Last name" />
-      <select {...register("category")}>
-        <option value="">Select...</option>
-        <option value="A">Category A</option>
-        <option value="B">Category B</option>
-      </select>
-      <p>{result}</p>
-      <input type="submit" />
+      <div className="mb-6">
+        <label
+          for="firstName"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          First name
+        </label>
+        <input
+          {...register("firstName")}
+          placeholder="First name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        />
+      </div>
+
+      <div className="mb-6">
+        <label
+          for="lastName"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          Last name
+        </label>
+        <input
+          {...register("lastName")}
+          placeholder="Last name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        />
+      </div>
+
+      <div className="mb-6">
+        <label
+          for="category"
+          className="block mb-2 text-sm font-medium text-white"
+        >
+          Category
+        </label>
+        <select
+          {...register("category")}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        >
+          <option value="">Select...</option>
+          <option value="A">Category A</option>
+          <option value="B">Category B</option>
+        </select>
+      </div>
+
+      <p className="text-white my-6">{result}</p>
+
+      <input
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full"
+      />
     </form>
   );
 };
--- netlify-forms-e2e-testing/src/pages/index.js
+++ netlify-forms-e2e-testing/src/pages/index.js
@@ -5,9 +5,13 @@ import FormComponent from "../components/form";
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Demo Project for E2E-Tests of Netlify Forms</h1>
-      <FormComponent />
+    <main className="bg-slate-900 min-h-screen">
+      <section className="container mx-auto p-40 flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-purple-500 mb-6">
+          Demo Project for E2E-Tests of Netlify Forms
+        </h1>
+        <FormComponent />
+      </section>
     </main>
   );
 };
